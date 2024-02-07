1cf143f84a53820bb1a5455304f5dd1c410cfc83
YaYaMonico <yasmiinmonico@gmail.com>
2024-02-07 20:40:09 -0300

    Ava-portal

---
+++ Ex_portal/Ava-portal.py
@@ -0,0 +1,10 @@
+lista_cantores = ['Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa']
+
+quant_elementos = len(lista_cantores)
+print("Quantidade de elementos na lista:", quant_elementos)
+
+print("Índice 2:", lista_cantores[2])
+
+print("Índice 9:", lista_cantores[9])
+
+print("Índice 14:", lista_cantores[14])
